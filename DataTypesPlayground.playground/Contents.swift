601be7c90b0fa364f412ce01c64fb19916b3b8ee
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-05 13:10:00 -0400

    Playing with data types and interpolation

---
+++ DataTypesPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+let thirdPlanet = "Earth"
+var ninthPlanet : String
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+let sixthPlanet = "Saturn"
+let seventhPlanet = "Uranus"
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
+
+let numberOfStatesInAmerica = 51
